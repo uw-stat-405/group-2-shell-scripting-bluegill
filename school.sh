4f85db90043de120ea6b2203dfe04da97d8d93b2
Michael Liou <lioumens@gmail.com>
2024-02-28 20:58:47 -0600

    updated passing command

--- school.sh
+++ school.sh
@@ -1,4 +1,18 @@
-# cat Property_Tax_Roll.csv | grep "MADISON SCHOOLS" | cut -d , -f 7 | paste -sd+ - | bc
+# cat Property_Tax_Roll.csv | grep "MADISON SCHOOLS" | cut -d , -f 7 | paste -sd+ - | bc)
 
 # This should fail the first test, but pass the second
-echo 24154170100
+# echo 24154170100
+# value=$(grep "MADISON SCHOOLS" < Property_Tax_Roll.csv | \
+#     cut -d , -f 7)
+value=$(cat Property_Tax_Roll.csv | \
+    grep "MADISON SCHOOLS" | \
+    cut -d , -f 7)
+
+n=$(echo "$value" | wc -l)
+sum=$(echo "$value"  | paste -sd+ - | bc)
+
+echo "scale=2; $sum/$n" | bc
+
+# One liner
+# grep "MADISON SCHOOLS" < Property_Tax_Roll.csv | \
+#     awk -F, '{sum += $7} END {if (NR > 0) printf "%.2f", sum / NR}'
