c8918d735382355f2e754c79b97fd266af46ea0e
Michael Liou <lioumens@gmail.com>
2024-03-01 09:17:07 -0600

    not triggering

---
+++ digits.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+# one liner
+# seq 1000 2000 | awk '/^[01]+$/ {sum += $0} END {print sum}'
+
+# pipe into brace expression
+# seq 1000 2000 | \
+#     {
+#         sum=0
+#         while read -r i; do
+#             if [[ $i =~ ^[01]+$ ]]; then
+#                 sum=$((sum + i))
+#             fi
+#         done
+#         echo $sum
+#     }
+
+# sum=0
+# for (( i=1000; i<=2000; i++)); do
+#     if [[ $i =~ ^[01]+$ ]]; then
+#         sum=$((sum + i))
+#     fi
+# done
+
+# i=1000
+# while [ $i -lt 2001 ]; do
+#     left=${i//[^01]}
+#     if [[ ${#left} -eq 4 ]]; then
+#         echo $i
+#     fi
+#     i=$((i+1))
+# done
+
+# echo $sum
