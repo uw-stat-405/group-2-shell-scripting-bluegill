606642b2c722a17311c8ec6b9770a6b29c40c54d
Michael Liou <lioumens@gmail.com>
2024-03-01 15:36:52 -0600

    updated check & school solution

--- school.sh
+++ school.sh
@@ -1,4 +1,18 @@
-# cat Property_Tax_Roll.csv | grep "MADISON SCHOOLS" | cut -d , -f 7 | paste -sd+ - | bc
+#!/bin/bash
+
+value=$(cat Property_Tax_Roll.csv | \
+    grep "MADISON SCHOOLS" | \
+    cut -d , -f 7)
+
+# value=$(grep "MADISON SCHOOLS" < Property_Tax_Roll.csv | \
+#     cut -d , -f 7)
+
+n=$(echo "$value" | wc -l)
+sum=$(echo "$value"  | \
+    paste -sd+ - | bc)
+# echo $sum
+
+echo "scale=2; $sum/$n" | bc
 
 # This should fail the first test, but pass the second
-echo 24154170100
+# echo 24154170100
